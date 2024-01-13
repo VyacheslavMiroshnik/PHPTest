9eb2825ffc39d100f1558bbfbf6b6f58b1ff85a0
Vyacheslav Miroshnik <mepa087@gmail.com>
2024-01-13 22:11:36 +0300

    init composer,add autoload,add 2 class User and UserTable,add 1 interface TableWrapper

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+require_once('vendor/autoload.php');
+use Vyacheslav\Classes\User;
+$user2 = new User();
+$user2->db->insert(['name' => 'John', 'email' => 'tugrp@example.com']);
+print_r($user2->db->get());
+echo "fsdfsdfsd";
